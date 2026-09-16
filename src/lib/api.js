664b809bba62d267d95c3f54cd9c731ff493d10b
samuel2602 <munozsamuel2602@gmail.com>
2026-09-16 07:52:23 -0500

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
-import { PUBLIC_API_URL } from '$env/static/public';
+const API_URL = import.meta.env.VITE_API_URL?.replace(/\/$/, '') || '';
 
-const API_URL = PUBLIC_API_URL.replace(/\/$/, '');
+
 
 export async function request(path, options = {}) {
   const response = await fetch(`${API_URL}${path}`, { headers: { 'Content-Type': 'application/json', ...options.headers }, ...options });
